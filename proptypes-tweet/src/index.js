8576d49519128146d4298f4d395a233a61fc92bd
Catriona Meriel <catriona@1partcarbon.co.uk>
2019-07-25 10:00:00 +0100

    Add proptypes exercises

---
+++ proptypes-tweet/src/index.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import './index.css';
+
+const testTweet = {
+  message: 'This is my tweet, what do you think?',
+  gravatar: "xyz",
+  author: {
+    handle: 'imahandle',
+    name: 'ILove Cats'
+  },
+  likes: 2,
+  retweets: 5,
+  timestamp: "2019-07-20 21:25:37"
+}
+
+function Tweet({ tweet }) {
+  return (
+    <div className="tweet">
+      <Avatar hash={tweet.gravatar}/>
+      <div className="content">
+        <NameWithHandle author={tweet.author} /><Time time={tweet.timestamp} />
+        <Message text={tweet.message} />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton retweets={tweet.retweets}/>
+          <LikeButton likes={tweet.likes}/>
+          <MoreOptionsButton />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Tweet.propTypes = {
+  tweet: PropTypes.shape({
+    message: PropTypes.string,
+    gravatar: PropTypes.string,
+    author: PropTypes.object.isRequired,
+    likes: PropTypes.number,
+    retweets: PropTypes.number,
+    timestamp: PropTypes.string
+  })
+}
+
+function Avatar({ hash }) {
+  const url = `https://www.gravatar.com/avatar/${hash}`;
+  return (
+    <img src={url}
+    className="avatar"
+    alt="avatar" />
+  )
+}
+
+Avatar.propTypes = {
+  hash: PropTypes.string
+}
+
+
+function Message({ text }) {
+  return (
+    <div className="message">
+      {text}
+    </div>
+  )
+}
+
+Message.propTypes = {
+  author: PropTypes.string
+}
+
+function NameWithHandle({ author }) {
+  return (
+    <span className="name-with-handle">
+      <span className="name">
+        {author.name}
+      </span>
+      <span className="handle">
+        @{author.handle}
+      </span>
+    </span>
+  )
+}
+
+NameWithHandle.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    handle: PropTypes.string.isRequired
+  }).isRequired
+}
+
+const Time = ({ time }) => {
+  const timeString = moment(time).fromNow();
+  return (
+    <span className="time">
+      {timeString}
+    </span>
+  );
+}
+
+Time.propTypes = {
+  time: PropTypes.string
+}
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button" />
+)
+
+const RetweetButton = ({ retweets }) => {
+  const retweetsCount = getCount(retweets);
+  return (
+    <span className="retweet-button">
+      <i className="fa fa-retweet" />
+      {retweetsCount}
+    </span>
+  )
+}
+
+RetweetButton.propTypes = {
+  retweets: PropTypes.number
+};
+
+const LikeButton = ({ likes }) => {
+  const likesCount = getCount(likes);
+  return (
+    <span className="like-button">
+      <i className="fa fa-heart" />
+      {likesCount}
+    </span>
+  )
+}
+
+LikeButton.propTypes = {
+  likes: PropTypes.number
+};
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+)
+
+const getCount = (count) => {
+  if (count > 0) {
+    return (
+      <span className="count">
+        {count}
+      </span>
+    );
+  } else {
+    return null;
+  }
+};
+
+ReactDOM.render(
+  <Tweet tweet={testTweet} />,
+  document.querySelector('#root')
+)
