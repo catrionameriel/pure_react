6ce8e38596c6eb8165e3a1399f27c330c8843616
Catriona Meriel <catriona@1partcarbon.co.uk>
2019-11-29 11:38:59 +0000

    Update tweets exercise to show multiple

--- proptypes-tweet/src/index.js
+++ proptypes-tweet/src/index.js
@@ -4,21 +4,56 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import './index.css';
 
-const testTweet = {
-  message: 'This is my tweet, what do you think?',
-  gravatar: "xyz",
-  author: {
-    handle: 'imahandle',
-    name: 'ILove Cats'
+const testTweets = [
+  {
+    message: 'This is my tweet, what do you think?',
+    gravatar: "xyz",
+    author: {
+      handle: 'imahandle',
+      name: 'ILove Cats'
+    },
+    likes: 2,
+    retweets: 5,
+    timestamp: "2019-07-20 21:25:37"
   },
-  likes: 2,
-  retweets: 5,
-  timestamp: "2019-07-20 21:25:37"
+  {
+    message: 'This is my second tweet, it sucks',
+    gravatar: "xyz",
+    author: {
+      handle: 'imahandle',
+      name: 'ILove Cats'
+    },
+    likes: 0,
+    retweets: 0,
+    timestamp: "2019-09-20 05:10:10"
+  },
+  {
+    message: 'Hello World!',
+    gravatar: "xyz",
+    author: {
+      handle: 'imahandle',
+      name: 'ILove Cats'
+    },
+    likes: 100,
+    retweets: 50,
+    timestamp: "2019-10-30 20:08:12"
+  }
+]
+
+function TweetList({ tweets }) {
+  return (
+    <ul>
+      {tweets.map((tweet) => {
+        return <Tweet tweet={tweet} />
+      })}
+    </ul>
+  )
 }
+
 
 function Tweet({ tweet }) {
   return (
-    <div className="tweet">
+    <li className="tweet">
       <Avatar hash={tweet.gravatar}/>
       <div className="content">
         <NameWithHandle author={tweet.author} /><Time time={tweet.timestamp} />
@@ -30,7 +65,7 @@ function Tweet({ tweet }) {
           <MoreOptionsButton />
         </div>
       </div>
-    </div>
+    </li>
   )
 }
 
@@ -153,6 +188,6 @@ const getCount = (count) => {
 };
 
 ReactDOM.render(
-  <Tweet tweet={testTweet} />,
+  <TweetList tweets={testTweets} />,
   document.querySelector('#root')
 )
